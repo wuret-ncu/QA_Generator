cd647b794af97367dba2dce6a2849e3aa7145841
yehtony <yehtony880208@outlook.com>
2023-04-28 18:57:48 +0800

    login/register

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
             <Route path='/read' element={<Read />}></Route>
             <Route path='/write' element={<Write />}></Route>
             <Route path='/analysis' element={<Analytic />}></Route>
-            <Route path='/logout' element={<Login />}></Route>
+            <Route path='/login' element={<Login />}></Route>
             <Route path='/register' element={<Register />}></Route>
         </Routes>
       </BrowserRouter>
