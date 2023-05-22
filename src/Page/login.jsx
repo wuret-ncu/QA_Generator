3fd2bc585e999bb99dce5112f4450369730e355a
LAPTOP-Q28U8QQ6\yexua <yexuanncu@gmail.com>
2023-05-22 22:33:28 +0800

    eatin在搞

--- src/Page/login.jsx
+++ src/Page/login.jsx
@@ -1,45 +1,78 @@
 import LoginButton from "./login/loginPage"
-import { useEffect, useContext,useState } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { gapi } from 'gapi-script';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom"
 import { Context } from "../Contexts/Context";
 
-
 function Login() {
-    const navigation = useNavigate()
+    const navigation = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
-    const {test, setTest} = useContext(Context)
-
+    const { test, setTest } = useContext(Context)
+    const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
     const handleTogglePassword = () => {
         setShowPassword(!showPassword);
     };
 
-    const handleLogin = () =>{
-        setTest(true)
-        navigation('/')
-    } 
-    // useEffect(() => {
-    //     function start() {
-    //         gapi.client.init({
-    //             clientId: clientId,
-    //             scope: ""
-    //         })
-    //     };
-    //     gapi.load('client:auth2', start);
-    // });
+    const handleLogin = () => {
+        const email = document.getElementById('email').value;
+        console.log(email)
+        const password = document.getElementById('password').value;
+        fetch('http://localhost:8003/api/user/check', {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.message === "Password is correct.") {
+                    localStorage.setItem("user", data.data);
+                    setTest(true);
+                    setSuccessMessage(data.message)
+                }
+                else {
+                    setErrorMessage(data.message);
+                    setShowAlert(true);
+                    // 3秒后隐藏警报
+                    setTimeout(() => {
+                        setShowAlert(false);
+                    }, 3000);
+                }
+            })
+            .catch(error => console.error(error));
+    };
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
+                            {successMessage && (
+                                <div className="alert alert-success shadow-lg">
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                        <span>{successMessage}</span>
+                                        <button className="btn btn-sm btn-primary" onClick={() => navigation("/")}>Accept</button>
+                                    </div>
+                                </div>)}
+                            {errorMessage && showAlert && (
+                                <div className=" alert alert-error shadow-lg">
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                                        <span>{errorMessage}</span>
+                                    </div>
+                                </div>
+                            )}
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input id="email" type="text" placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -47,6 +80,7 @@ function Login() {
                                 </label>
                                 <div style={{ position: "relative" }} className="flex items-center">
                                     <input
+                                        id="password"
                                         type={showPassword ? "text" : "password"}
                                         placeholder="password"
                                         className="input input-bordered pr-12"
@@ -67,21 +101,20 @@ function Login() {
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary" onClick={()=> handleLogin()}>Login</button>
+                                <button className="btn btn-primary" onClick={handleLogin}>Login</button>
                             </div>
                             <div className="divider">OR</div>
                             <LoginButton />
                         </div>
                     </div>
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Welcome to <b className="text-primary">QA Generator</b> !</h1>
-                        <p className="py-6">Test your English skills through our <b className="text-secondary">reading test </b>and
-                            <b className="text-secondary"> writing assessments</b>. With a variety of question types and difficulty levels, receive instant feedback and navigate with ease. Improve your English skills today by starting now!</p>
+                        <h1 className="text-5xl font-bold">Welcome to <b className="text-primary">QA Generator</b>!</h1>
+                        <p className="py-6">Test your English skills through our <b className="text-secondary">reading test</b> and <b className="text-secondary">writing assessments</b>. With a variety of question types and difficulty levels, receive instant feedback and navigate with ease. Improve your English skills today by starting now!</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Login;
