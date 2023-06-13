fc1c23f6c4b76bd92ffff6f0a3b2510b4b9b0439
LAPTOP-Q28U8QQ6\yexua <yexuanncu@gmail.com>
2023-06-14 00:10:50 +0800

    大報社 openai

---
+++ src/API/openaiFinder.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+// export default axios.create({ baseURL: "http://localhost:5006/api/v1/school",});
+export default axios.create({ baseURL: "https://qag02.openai.azure.com/",});
--- src/Page/writePage.jsx
+++ src/Page/writePage.jsx
@@ -8,7 +8,15 @@ import { data } from 'autoprefixer';
 import { useNavigate } from "react-router-dom"
 import { Context } from '../Contexts/Context';
 import { WritingContext } from '../Contexts/writingContext';
+import axios from 'axios';
+import openaiFinder from '../API/openaiFinder';
+const { Configuration, OpenAIApi } = require("openai");
 
+const configuration = new Configuration({
+  apiKey: '17f82d1fc6fe4d0ba2a768d8836c3e89',
+});
+
+const openai = new OpenAIApi(configuration);
 
 const options = [
   { value: 'collegeEntranceExam', label: '學測' },
@@ -42,7 +50,34 @@ function WritingPage() {
   const handleCriteriaChange = (value) => {
     setCriteria(value);
     console.log(value)
+    //callopenaiapi
+    openaiFinder.post(
+      'https://qag02.openai.azure.com/openai/deployments/QAG02/completions?api-version=2022-12-01&api-key=1343d3e41dd14a498bb8461abd5d59dc',
+      {
+        prompt:
+          'Generate a multiple choice quiz from the text below. Quiz should contain at least 5 questions. Each answer choice should be on a separate line, with a blank line separating each question.\n\nA neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich. Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars. Neutron stars have a radius on the order of 10 kilometers (6.2 mi) and a mass of about 1.4 solar masses. They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.\n\nExample:\nQ1. What is a neutron star?\nA. The collapsed core of a massive supergiant star\nB. The smallest and densest stellar object\nC. A white hole\nD. A quark star',
+        temperature: 0.8,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.5,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then(response => {
+        // 解析回傳的資料，並設定quizQuestions狀態
+        const questions = response.data.choices[0].text
+        console.log(questions)
+      })
+      .catch(error => {
+        console.error('123');
+      })
   };
+  //callopenaiapi
 
   const handleTopicChange = (value) => {
     setTopic(value);
@@ -60,6 +95,7 @@ function WritingPage() {
     setWordCount(essay.split(/\s+/).filter((word) => word !== '').length);
   };
 
+  // const [quizQuestions, setQuizQuestions] = useState([]);
   useEffect(() => {
     Finder.get('http://localhost:8003/api/WriteArticle/getTopic', {
       headers: { 'Content-Type': 'application/json' }
